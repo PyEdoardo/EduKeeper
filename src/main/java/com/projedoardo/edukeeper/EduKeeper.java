363cee3151eb0770f2d5c0cce35003f72604554d
Edoardo Tombolesi <42304654+PyEdoardo@users.noreply.github.com>
2024-08-28 02:02:08 -0300

    Add files via upload

---
+++ src/main/java/com/projedoardo/edukeeper/EduKeeper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.projedoardo.edukeeper;
+
+/**
+ *
+ * @author edoar
+ */
+public class EduKeeper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
